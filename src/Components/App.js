e3ebb7b7cd98e8f708089452e60a3db3c30e445e
DanNisenson <danpablo1@gmail.com>
2023-02-01 20:59:52 +0100

    move routes to App component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,27 @@
-import NavBar from './NavBar';
-import Help from '../Pages/Help.jsx';
-import { BrowserRouter } from 'react-router-dom';
-import CssBaseline from '@mui/material/CssBaseline';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Business from '../Pages/Business';
+import Developer from '../Pages/Developer';
+import Help from '../Pages/Help';
+import Home from '../Pages/Home';
+import Login from '../Pages/Login';
+import Personal from '../Pages/Personal';
+import Signup from '../Pages/Signup';
+import NavBar from "./NavBar";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-      {/* <CssBaseline/> */}
-      <NavBar/>
+        <NavBar />
+				<Routes>
+                <Route path='' element={<Home />} />
+                <Route path='Personal' element={<Personal />} />
+                <Route path='Business' element={<Business />} />
+                <Route path='Developer' element={<Developer />} />
+                <Route path='Help' element={<Help />} />
+                <Route path='Login' element={<Login />} />
+                <Route path='Signup' element={<Signup />} />
+            </Routes>
       </BrowserRouter>
     </div>
   );
--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,6 +1,10 @@
+import { useEffect, useState } from "react";
 import '../Styles/Signup.css';
 
 export default function Signup () {
+    const [accountType, setAccountType] = useState("personal");
+    const [page, setPage] = useState(1);
+
     return(
         <div>
             
