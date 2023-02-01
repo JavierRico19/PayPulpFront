25186ad37465c8ff918a3cc3ff0ebb81d1f492fd
DanNisenson <danpablo1@gmail.com>
2023-02-01 21:17:59 +0100

    wip

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,29 +1,29 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Business from '../Pages/Business';
-import Developer from '../Pages/Developer';
-import Help from '../Pages/Help';
-import Home from '../Pages/Home';
-import Login from '../Pages/Login';
-import Personal from '../Pages/Personal';
-import Signup from '../Pages/Signup';
+import Business from "../Pages/Business";
+import Developer from "../Pages/Developer";
+import Help from "../Pages/Help";
+import Home from "../Pages/Home";
+import Login from "../Pages/Login";
+import Personal from "../Pages/Personal";
+import Signup from "../Pages/Signup";
 import NavBar from "./NavBar";
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <NavBar />
-				<Routes>
-                <Route path='' element={<Home />} />
-                <Route path='Personal' element={<Personal />} />
-                <Route path='Business' element={<Business />} />
-                <Route path='Developer' element={<Developer />} />
-                <Route path='Help' element={<Help />} />
-                <Route path='Login' element={<Login />} />
-                <Route path='Signup' element={<Signup />} />
-            </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <div className="main">
+        <Routes>
+          <Route path="" element={<Home />} />
+          <Route path="Personal" element={<Personal />} />
+          <Route path="Business" element={<Business />} />
+          <Route path="Developer" element={<Developer />} />
+          <Route path="Help" element={<Help />} />
+          <Route path="Login" element={<Login />} />
+          <Route path="Signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,13 +1,19 @@
+import { Typography } from "@mui/material";
 import { useEffect, useState } from "react";
-import '../Styles/Signup.css';
+import "../Styles/Signup.css";
 
-export default function Signup () {
-    const [accountType, setAccountType] = useState("personal");
-    const [page, setPage] = useState(1);
+export default function Signup() {
+  const [accountType, setAccountType] = useState("personal");
+  const [page, setPage] = useState(1);
 
-    return(
-        <div>
-            
-        </div>
-    )
+  return <div>
+    {page === 1 && 
+  <div className="main">
+    <div className="info">
+    <Typography>Sign up to PayPulp!</Typography>
+    </div>
+    <div></div>
+  </div>
+  }
+  </div>;
 }
